9ffbf29a6a767f48d34c41e6fa345d7891b41f52
Vishal Gorana <vishalgorana514@gmail.com>
2023-04-29 17:00:49 +0530

    daf

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded());
 app.use(bodyParser.json())
 
 mongoose.set('strictQuery', true);
