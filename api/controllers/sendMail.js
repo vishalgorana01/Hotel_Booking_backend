8f8d900baa42a188466e7c2d9dde72276313d431
Vishal Gorana <vishalgorana514@gmail.com>
2023-06-04 19:34:32 +0530

    smtp server changed

--- api/controllers/sendMail.js
+++ api/controllers/sendMail.js
@@ -3,25 +3,26 @@ const nodemailer = require('nodemailer');
 const sendMail = async(email, Reset_id)=> {
     // connect with smtp; 
     const transporter = await nodemailer.createTransport({
-        host: 'smtp.elasticemail.com',
-        port: 2525,
+        host: 'smtp.office365.com',
+        port: 587,
         auth: {
-            user: 'hotelio43@gmail.com',
-            pass: 'B33EE27E24F25ED14A84EE08CF3E50B4DF8E'
+            user: 'hotelio43@outlook.com',
+            pass: 'owner_hotelio_43'
         }
     });
     
     let mailDetails = {
-        from: 'hotelio43@gmail.com',
+        from: 'hotelio43@outlook.com',
         to: `${email}`,
         subject: 'Reset Password',
         text: `Click on the below to Reset Your Password :- 
-        http://localhost:3000/ResetPassword/?id=${Reset_id}`,
+        https://hotel-booking-frontend-zeta.vercel.app/ResetPassword/?id=${Reset_id}`,
     };
     
     transporter.sendMail(mailDetails, function(err, data) {
         if(err) {
             console.log(err);
+            
         } else {
             console.log('Email sent successfully');
         }
